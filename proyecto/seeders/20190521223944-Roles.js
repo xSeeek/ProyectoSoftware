bfe756d51c8f09a2c56a0ab3bd5f257d775ed4de
xSeeek <francisco.riquelme@outlook.com>
2019-05-26 21:41:52 -0400

    Fix SEED

--- proyecto/seeders/20190521223944-Roles.js
+++ proyecto/seeders/20190521223944-Roles.js
@@ -5,8 +5,8 @@ module.exports = {
       return queryInterface.bulkInsert('Rol', [{
         tipo: 'Desarrollador',
         descripcion: 'Desarrollador del Sistema',
-        createdAt: sequelize.fn('NOW'),
-        modifiedAt: sequelize.fn('NOW')
+        createdAt: new Date(),
+        updatedAt: new Date()
       }], {});
   },
 
