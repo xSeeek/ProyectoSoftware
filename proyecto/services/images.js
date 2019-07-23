6d4eae1f5860c8bb38247d0e9d2ade3c8399a1a1
Francisco Riquelme Cavagnola <francisco.riquelme@outlook.com>
2019-07-22 20:02:07 -0400

    Test Blob 1

--- proyecto/services/images.js
+++ proyecto/services/images.js
@@ -4,26 +4,27 @@ const path = require('path');
 const filesDir = './data/profiles/';
 
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, filesDir);
-    },
-    filename: function (req, file, cb) {
-        var filename = file.originalname;
-        if(req.body.photoName != null && req.body.photoName != "")
-        {
-          fs.readdir(filesDir, (err, files) => {
-            if (err) throw err;
-            for (const file of files)
-              if(file.split(".")[0] == req.body.photoName)
-                fs.unlink(path.join(filesDir, file), err => {
-                    if (err) throw err;
-                });
-          });
-          cb(null, req.body.photoName + path.extname(filename));
-        }
-        else
-          cb(null, Date.now() + path.extname(filename));
-    }
+  destination: (req, file, cb) => {
+    console.log(file.mimetype);
+    cb(null, filesDir);
+  },
+  filename: function (req, file, cb) {
+      var filename = file.originalname;
+      if(req.body.photoName != null && req.body.photoName != "")
+      {
+        fs.readdir(filesDir, (err, files) => {
+          if (err) throw err;
+          for (const file of files)
+            if(file.split(".")[0] == req.body.photoName)
+              fs.unlink(path.join(filesDir, file), err => {
+                  if (err) throw err;
+              });
+        });
+        cb(null, req.body.photoName + path.extname(filename));
+      }
+      else
+        cb(null, Date.now() + path.extname(filename));
+  }
 });
 
 var uploadPhoto = multer({storage: storage});
