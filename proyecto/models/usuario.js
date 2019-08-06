0abc4c4edcfeed143b872a2a09803fa0ec7eff15
Francisco Riquelme Cavagnola <42775834+xSeeek@users.noreply.github.com>
2019-08-06 18:35:07 -0400

    AllowNull for password

--- proyecto/models/usuario.js
+++ proyecto/models/usuario.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: true
     },
     estado: {
       type: DataTypes.INTEGER
